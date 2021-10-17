0dbb22e47dde39d69f0b90956cca526c87fc980f
Janduhy F <Janduhyfinizola15@gmail.com>
2021-10-17 10:49:25 -0300

    Projeto Estagio

---
+++ Desafio/database/migrations/2021_10_16_040953_create_carros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Carros', function (Blueprint $table) {
+            $table->id();
+            $table->string('modelo');
+            $table->string('marca');
+            $table->string('tipo');
+            $table->boolean('status')->nullable($value = true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carros');
+    }
+}
